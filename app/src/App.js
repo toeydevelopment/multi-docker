bfbe496ef0d1df7b07a16200c7ef896910eb1ce1
Anirut Kamchai <anirut.k@cdg.co.th>
2019-11-09 21:20:49 +0700

    - [feat] add new react v2

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
@@ -11,7 +11,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
+            {/* <img src={logo} className="App-logo" alt="logo" /> */}
             <h1 className="App-title">Welcome to React</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
